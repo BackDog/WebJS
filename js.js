a02c7ff8ee223a0f8fcd91a0da500cb3d13e8677
BackDog <canhnt1204@gmail.com>
2018-02-05 22:37:22 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
-function myAlert(){
-  alert("Hello");
+function myFunction() {
+    var x = document.createElement("BUTTON");
+    var t = document.createTextNode("Click me");
+    x.appendChild(t);
+    document.body.appendChild(x);
 }
